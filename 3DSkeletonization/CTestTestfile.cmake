84df93d203c5ed4692ca1df750bb4ec1bc8dba3a
Arnaud <arnaudconver7@gmail.com>
2019-05-07 19:49:08 +0200

    set up gitignore

--- 3DSkeletonization/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/hanane/TER/projetGithub/Projet_Ter/3DSkeletonization
-# Build directory: /home/hanane/TER/projetGithub/Projet_Ter/3DSkeletonization
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("Code")
